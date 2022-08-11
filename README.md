aa1411b35c9cdda2ff2afdc8d7ca29376afb6ea6
bhagavath <lordcodeking@gmail.com>
2022-08-12 01:48:14 +0530

    lab3

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 - [lab1](https://github.com/lordcod99/DBMS_LABS/tree/main/lab1) : A Dummy website using only HTML and CSS
 - [lab2](https://github.com/lordcod99/DBMS_LABS/tree/main/lab2) : A Dummy database
+- [lab3](https://github.com/lordcod99/DBMS_LABS/tree/main/lab3) :
---
+++ lab3/lab3.sql
@@ -0,0 +1,98 @@
+-- create database  lab3;
+-- use lab3; 
+
+-- create table emp(
+-- 	user_id int not null,
+--     experience int not null,
+--     age int,
+--     gender varchar(7),
+--     salary int deafault 50000
+--    --  primary key(user_id) 
+--     
+-- );
+
+-- create table project(
+-- 	id int not null,
+--     name varchar(100),
+--     primary key(id)
+-- );
+
+
+-- using insert function to populate the data 
+
+-- insert into project 
+-- values(5,"anewara");
+
+-- using laod to populate the data 
+
+-- load data local infile "C:/Users/Sai/Downloads/Employee_Salary_Dataset.csv" into table emp
+-- FIELDS TERMINATED BY ','
+-- LINES TERMINATED BY '\n';
+
+-- adding constraints 
+
+
+-- alter table emp 
+-- add column project int default 1;
+
+-- alter table emp
+-- add constraint pk primary key(user_id);
+
+-- alter table emp
+-- add constraint fk foreign key(project) references project(id);
+
+-- modifying the constraints 
+
+-- alter table emp 
+-- ALTER salary SET DEFAULT 60000;
+
+-- droping the constraints 
+
+
+-- alter table emp
+-- drop constraint fk;
+
+-- odifying the constraints by first drpoing and adding as new modified constraint
+
+-- alter table emp
+-- add constraint fk foreign key(project) references project(id)
+-- on delete set NULL;
+
+-- deleting the table entries
+
+-- delete from emp where user_id = 7;
+
+-- updating the table entries 
+
+-- update emp
+-- set project = 5
+-- where user_id = 9;
+
+
+
+
+
+select * from emp;
+
+select * from project;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
